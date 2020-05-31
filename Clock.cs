b8000bd0747778aa5f3d8f394fdfa2bedcd214c8
George Squair <george.squair@hesa.ac.uk>
2020-05-31 18:13:22 +0100

    First commit clock-angle

---
+++ Clock.cs
@@ -0,0 +1,66 @@
+using System;
+
+namespace clock_angle
+{
+    public class Clock
+    {
+        //Constructor takes in hourHand and minuteHand
+        public Clock(int hourHand, int minuteHand)
+        {
+            //Basic validation to ensure range is correct
+            if (minuteHand < 0 || minuteHand > 60){
+                throw new Exception("Minute value must be between 0 and 60");
+            } 
+
+            if (hourHand < 1 || hourHand > 24){
+                throw new Exception("Hour value must be between 1 and 24");
+            }
+
+            //If 24 hour clock is entered, convert to 12 hour
+            this.hourHand = hourHand > 12 ? hourHand - 12 : hourHand;
+
+            //0 and 60 are in same position for minutes, for instance cant have 16:60 as this is actually 17:00
+            if (minuteHand == 60) {
+                minuteHand = 0;
+                this.hourHand++;
+            }
+
+            this.minuteHand = minuteHand;
+        }
+
+        public int minuteHand { get; set; }
+        public int hourHand { get; set; }
+
+        private const int wholeClockDegrees = 360;
+        private const int clockSegments = 12;
+
+        public double findAngle(bool acute = true)
+        {
+            //Find number of segments minute hand uses then calculate total degree of angle
+            var minuteHandSegments = findSegments(this.minuteHand);
+            var minuteDegrees = findDegrees(minuteHandSegments);
+
+            //Get the fraction to modify the hour hands degree value
+            var angleModifier = (minuteDegrees / wholeClockDegrees) * (wholeClockDegrees / clockSegments);
+            var hourDegrees = findDegrees(this.hourHand) + angleModifier;
+
+            //Take absolute value to find difference between two angles
+            var angle = Math.Abs(hourDegrees - minuteDegrees);
+            
+            //Return acute depending on acute parameter = true otherwise return obtuse
+            return acute == true && (angle <= wholeClockDegrees / 2) ? angle : (wholeClockDegrees - angle);
+        }
+
+        private float findSegments(int hand)
+        {
+            //5 minutes in one segement
+            return (float)hand / 5;
+        }
+
+        private double findDegrees(double segments)
+        {  
+            return segments * (wholeClockDegrees / clockSegments);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace clock_angle
+{
+    //This program, when given a clocks hour and minute will calculate the angle between both hands.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Driver data
+            var clocks = new List<Clock> {
+                new Clock(6, 00), //6:00 am/pm
+                new Clock(3, 00), //3:00 am/pm
+                new Clock(4, 45), //4:45 am/pm
+                new Clock(16, 46), //16:45 pm
+                new Clock(4, 60) //4:60 which is actually 5:00am/pm
+            };
+
+            clocks.ForEach(clock =>
+            {
+                Console.WriteLine("At time {0}:{1}am/pm", clock.hourHand, clock.minuteHand);
+
+                //Call for acute angle
+                Console.WriteLine("Acute: {0} degrees", clock.findAngle());
+
+                //Call for obtuse angle
+                Console.WriteLine("Obtuse: {0} degrees", clock.findAngle(false));
+            });
+
+        }
+    }
+}
